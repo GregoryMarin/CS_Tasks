46a42e04580decbb728ed29ef0a867021b6b09c7
Gregory <gpost774@gmail.com>
2023-02-23 02:07:24 +0300

    Writed code

--- NewProject2_BootCamp/Program.cs
+++ NewProject2_BootCamp/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+int[] GetRangeSum(int[] array, int m)
+{
+    int n = array.Length;
+    int[] t = new int[n - m + 1];
+    int index = 0;
+    for (int i = 0; i <= n-m; i++)
+    {
+        for (int j = 0; j < i + m; j++)
+        {
+            t[index] += array[j];
+        }
+        index++;
+    }
+    return t;
+}
+
+int[] CreateArray(int size) => new int [size];
+
+string Print(int[] array) => $"[{String.Join(", ", array)}]";
+
+void Fill(ref int[] array) => array = array.Select(e => Random.Shared.Next(10)).ToArray();
+
+int[] numbers = CreateArray(15);
+
+Fill(ref numbers);
+
+Console.WriteLine(Print(numbers));
+
+int count = 5;
+int[] groupSumNumbers = GetRangeSum(numbers, count);
+Console.WriteLine(Print(groupSumNumbers));
+
